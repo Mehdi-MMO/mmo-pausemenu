b8ca02decc06dadaf79082ba03f0e2afa8a1042d
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-12-24 11:54:38 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+client_scripts {
+ 'client/*.lua'
+}
+
+server_scripts {
+ 'server/*.lua'
+}
+
+ui_page {
+ 'html/index.html', 
+}
+
+files {
+ 'html/index.html',
+ 'html/app.js', 
+ 'html/style.css'
+} 
